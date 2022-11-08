a1f78dfec52e82eb845fbedd10d069a7541b82a8
RyC2014 <rclingler1986@gmail.com>
2022-11-08 10:02:03 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(routes);
+
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
